835e29e7bfccd7d894128e09271642e6dc4c301d
Mark Miranda <notmarkmiranda@gmail.com>
2016-02-12 05:15:27 -0700

    more keygen work

--- test/keygen_test.rb
+++ test/keygen_test.rb
@@ -4,4 +4,9 @@ require 'minitest/pride'
 require 'pry'
 require_relative '../lib/keygen'
 
-class Keygen < Minitest::Test
+class Keygen_test < Minitest::Test
+  def test_it_generates_a_five_digit_number
+    key = Keygen.generate
+    assert_includes 10000..99999, key
+  end
+end
