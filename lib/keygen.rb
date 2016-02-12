fc6305a16fc988ce3eb69bc29af7fd860781529a
Mark Miranda <notmarkmiranda@gmail.com>
2016-02-11 18:31:46 -0700

    started keygen work

---
+++ lib/keygen.rb
@@ -0,0 +1,20 @@
+class Keygen
+
+  def self.generate
+    rand(9999..100000)
+  end
+
+key = Keygen.generate
+p key
+p key.to_s
+key1 = key.to_s[0..1]
+key2 = key.to_s[1..2]
+key3 = key.to_s[2..3]
+key4 = key.to_s[3..4]
+
+p key1.to_i
+p key2.to_i
+p key3.to_i
+p key4.to_i
+
+end
---
+++ test/keygen_test.rb
@@ -0,0 +1,7 @@
+require 'minitest'
+require 'minitest/autorun'
+require 'minitest/pride'
+require 'pry'
+require_relative '../lib/keygen'
+
+class Keygen < Minitest::Test
