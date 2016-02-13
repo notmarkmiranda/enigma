162b55c2e8fb6a59ceed0862a91c4ebb2a4d6b04
Mark Miranda <notmarkmiranda@gmail.com>
2016-02-13 15:25:27 -0700

    updated enigma, working on converting

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -8,28 +8,18 @@ class Enigma
   end
 
   def encrypt(message, key, date)
-    #format the key
-    @key = key
-    split_key = @key.chars
-    final_key = []
-    (0..3).each do |num|
-      final_key << split_key[num] + split_key[num + 1]
-    end
-    final_key = final_key.map do |key|
-      key.to_i
-    end
+    format_the_key(key)
+    format_the_offset(date)
+    add_them_together
 
-    # format the offset
-    @date = date.to_i
-    calc_date = (@date * @date).to_s
-    calc_date = calc_date.chars[-4..-1]
-    final_offset = calc_date.map do |offset|
-      offset.to_i
+    # convert the message
+    indexed_characters = []
+    message.chars.each do |letter|
+      indexed_characters << @alphabet.find_index(letter)
     end
-
-    #add them together
-    code = %w(final_key, final_offset)
-    complete_code = [final_key, final_offset].transpose.map{ |x| x.reduce :+}
+    indexed_characters.each do |character|
+      
+    end
   end
 
   def decrypt
@@ -38,8 +28,33 @@ class Enigma
   def crack
   end
 
+# helper methods
+  def format_the_key(key)
+    @key = key
+    split_key = @key.chars
+    @final_key = []
+    (0..3).each do |num|
+      @final_key << split_key[num] + split_key[num + 1]
+    end
+    @final_key = @final_key.map do |key|
+      key.to_i
+    end
+  end
+
+  def format_the_offset(date)
+    @date = date.to_i
+    calc_date = (@date * @date).to_s
+    calc_date = calc_date.chars[-4..-1]
+    @final_offset = calc_date.map do |offset|
+      offset.to_i
+    end
+  end
+
+  def add_them_together
+    @complete_code = [@final_key, @final_offset].transpose.map{ |x| x.reduce :+}
+  end
+
   e = Enigma.new
-  my_message = "this is my message ..end.."
+  my_message = "abc def"
   output = e.encrypt(my_message, "12345", "021316")
-  puts output
 end
--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -4,7 +4,23 @@ require 'minitest/autorun'
 require 'pry'
 require_relative '../lib/enigma'
 
-class Enigma_test
+class Enigma_test < Minitest::Test
 
-  
+  def test_format_key_creates_4_digit_array
+    e = Enigma.new
+    assert_equal [12, 23, 34, 45], e.format_the_key("12345")
+  end
+
+  def test_format_the_date_correctly
+    e = Enigma.new
+    assert_equal [1, 8, 5, 6], e.format_the_offset("021316")
+  end
+
+  def test_can_calculate_final_key
+    e = Enigma.new
+    output = e.encrypt("message", "12345", "021316")
+    @complete_code
+
+  end
+
 end
