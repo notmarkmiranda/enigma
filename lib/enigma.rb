76515e2b6534f3299c3d082a539a35c8611c8bff
Mark Miranda <notmarkmiranda@gmail.com>
2016-02-13 14:28:50 -0700

    rewriting into one file

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,21 +1,45 @@
+require 'date'
 
 class Enigma
-attr_reader :message, :key, :offset
-# output = e.encrypt(my_message, "12345", Date.today)
-  def initialize(message = nil, key = nil, offset = nil)
-    @message = message
+  attr_reader :alphabet
+
+  def initialize
+    @alphabet = %w(a b c d e f g h i j k l m n o p q r s t u v w x y z 0 1 2 3 4 5 6 7 8 9 \  . ,)
+  end
+
+  def encrypt(message, key, date)
+    #format the key
     @key = key
-    @offset = offset
+    split_key = @key.chars
+    final_key = []
+    (0..3).each do |num|
+      final_key << split_key[num] + split_key[num + 1]
+    end
+    final_key = final_key.map do |key|
+      key.to_i
+    end
+
+    # format the offset
+    @date = date.to_i
+    calc_date = (@date * @date).to_s
+    calc_date = calc_date.chars[-4..-1]
+    final_offset = calc_date.map do |offset|
+      offset.to_i
+    end
+
+    #add them together
+    code = %w(final_key, final_offset)
+    complete_code = [final_key, final_offset].transpose.map{ |x| x.reduce :+}
   end
 
-  def encrypt(message, key = nil, offset = nil)
-    p message
-    p key
-    p offset
+  def decrypt
   end
 
-e = Enigma.new
-my_message = "This is a secret ..end.."
-output = e.encrypt(my_message)
+  def crack
+  end
 
+  e = Enigma.new
+  my_message = "this is my message ..end.."
+  output = e.encrypt(my_message, "12345", "021316")
+  puts output
 end
--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -6,8 +6,5 @@ require_relative '../lib/enigma'
 
 class Enigma_test
 
-  def test_it_can_receive_a_message_key_date
-    
-  end
-
+  
 end
