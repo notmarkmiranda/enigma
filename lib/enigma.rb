1e401fe8713d1a9a9977766a34eecaa83b493485
Mark Miranda <notmarkmiranda@gmail.com>
2016-02-14 11:49:55 -0700

    starting keygenerator

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -7,7 +7,7 @@ class Enigma
     @alphabet = %w(a b c d e f g h i j k l m n o p q r s t u v w x y z 0 1 2 3 4 5 6 7 8 9 \  . ,)
   end
 
-  def encrypt(message, key, date)
+  def encrypt(message, key=nil, date=nil)
     format_the_key(key)
     format_the_offset(date)
     add_them_together
@@ -16,7 +16,7 @@ class Enigma
     convert_back_join
   end
 
-  def decrypt(message, key, date)
+  def decrypt(message, key, date=nil)
     format_the_key(key)
     format_the_offset(date)
     add_them_together
@@ -34,7 +34,7 @@ class Enigma
     @encrypted_message.map.with_index do |letter, i|
       @final_message << (@alphabet[@encrypted_message[i] % @alphabet.size])
     end
-    p @final_message = @final_message.join
+    @final_message = @final_message.join
   end
 
   def encrypt_shift
@@ -48,7 +48,6 @@ class Enigma
     @indexed_characters.map.with_index do |in_ch, i|
       @encrypted_message << (in_ch - @complete_code[i % @complete_code.size])
     end
-    @encrypted_message
   end
 
   def convert_the_message_to_index(message)
@@ -61,6 +60,9 @@ class Enigma
 
   def format_the_key(key)
     @key = key
+    if @key == nil
+      @key = generate_key
+    end
     split_key = @key.chars
     @final_key = []
     (0..3).each do |num|
@@ -71,13 +73,29 @@ class Enigma
     end
   end
 
+  def generate_key
+    @key = rand(10000..99999)
+    @key = @key.to_s
+  end
+
   def format_the_offset(date)
-    @date = date.to_i
+    @date = date
+    if @date == nil
+      @date = create_date
+    end
+    @date = @date.to_i
     calc_date = (@date * @date).to_s
     calc_date = calc_date.chars[-4..-1]
     @final_offset = calc_date.map do |offset|
       offset.to_i
     end
+  end
+
+  def create_date
+    @date = []
+    today = Date.today
+    @date.push(today.mon.to_s, today.mday.to_s, today.year.to_s[2], today.year.to_s[3])
+    @date = @date.join
   end
 
   def add_them_together
@@ -86,6 +104,7 @@ class Enigma
 
   e = Enigma.new
   my_message = "abc def ghi"
-  output = e.encrypt(my_message, "12345", "021316")
-  # input = e.decrypt(output, "12345", "021316")
+  e.generate_key
+  p output = e.encrypt(my_message, "12345", "120681")
+  p input = e.decrypt(output, "12345", "120681")
 end
